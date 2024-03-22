3155d7699aa6d7d710d8ccf1fd09213c839b0d6a
Guy Lomax <G.Lomax@exeter.ac.uk>
2024-03-22 17:44:29 +0000

    Moved functions to separate file. Added section to calculate nonforest patch size.

--- scripts/sample_polygons.R
+++ scripts/sample_polygons.R
@@ -1,147 +1,76 @@
-## Script to generate hexagonal or square grids of a specified diameter covering
-## a country polygon.
+## Script to generate and sample hexagonal or square grids of a specified area
+## and minimum forest cover within a country polygon.
 
 library(sf)
 library(terra)
+library(landscapemetrics)
 library(here)
 library(dplyr)
 library(tmap)
+
+source(here("scripts", "R", "generate_polygons.R"))
 
 ## Set parameters --------
 
 COUNTRY <- "Colombia"
 POLY_SIZE <- 10000 # hectares
 POLY_SHAPE <- "hex"  # square or hex
-SAMPLE_N <- 1000  # Number of sample polygons
+SAMPLE_N <- 1000  # number of sample polygons
+FC_THRESHOLD <- 40  # Minimum forest cover to include polygons in sample (%)
+OUTPUT_PATH <- here("data", "processed", "vector", "sample_polygons")
+SAMPLE_SEED <- 111
 
 ## Load country polygon --------
 
 path <- here("data", "raw", "vector", "WB_countries_Admin0_10m", "WB_countries_Admin0_10m.shp")
-
-get_country <- function(path, country_name) {
-  countries <- st_read(path)
-  country <- dplyr::filter(countries, NAME_EN == country_name)
-  
-  country
-}
-
 country <- get_country(path, country_name = COUNTRY)
 
 # Convert to equal area projection (Cylindrical equal area)
 country_ea <- st_transform(country, crs = "ESRI:54034")
-## NEED TO ADD MERIDIANS, EASTINGS, NORTHINGS ETC
 
 ## Generate polygon grids --------
 
-generate_polygons <- function(geometry, shape, area) {
-  # Set arg to TRUE if shape == "square", FALSE if shape == "hex", else NA
-  square_arg = ifelse(
-    tolower(shape) == "square", TRUE, ifelse(
-      tolower(shape) == "hex", FALSE, NA))
-  
-  # Set area
-  area_ha <- units::set_units(area, "hectares")
-  area_km2 <- units::set_units(area_ha, "km ^ 2")
-  
-  # Generate grid
-  grid <- st_make_grid(
-    x = geometry,
-    cellsize = area_km2,
-    what = "polygons",
-    square = square_arg
-  )
-  
-  # Filter to those contained by geometry
-  
-  grid_contained <- grid %>%
-    st_as_sf() %>%
-    st_filter(geometry, .predicate = st_within)
-  
-  grid_contained
-}
-
-# Filter polygons to those contained by country boundaries
-
 grid <- generate_polygons(country_ea, shape = POLY_SHAPE, area = POLY_SIZE)
 
-tm_shape(country) + tm_fill("grey95") +
-  tm_shape(grid) + tm_borders("forestgreen")
-
-# ## Import forest/nonforest layer from MPC --------
-# library(rstac)
-# library(rsi)
-# 
-# stac_source <- stac("https://planetarycomputer.microsoft.com/api/stac/v1")
-# 
-# stac_search(
-#   q = stac_source,
-#   collections = "alos-fnf-mosaic",
-#   datetime = "2015-01-01/2020-01-01",
-#   bbox = st_bbox(country) %>% as.vector(),
-#   limit = 999
-# ) %>%
-#   get_request()
-# 
-# 
-# nc_fnf <- get_stac_data(
-#   # Spatial AOI:
-#   aoi = country_ea,
-#   # Temporal AOI:
-#   start_date = "2020-01-01",
-#   end_date = "2021-12-31",
-#   # Which asset do we want, from which collection, from which API:
-#   asset_names = "C",
-#   stac_source = "https://planetarycomputer.microsoft.com/api/stac/v1",
-#   collection = "alos-fnf-mosaic",
-#   # Where to save the file:
-#   output_filename = here("data", "raw", "raster", "palsar_colombia_2020.tif")
-# )
-# 
-# nc_fnf_rast <- rast(nc_fnf)
-
-## Filter polygons to those with > 40% forest cover --------
+## Calculate distance to forest edge per pixel --------
 
 # Load forest cover raster
+# Raster should be "closed", i.e., not contain small holes
 fc <- rast(here("data", "raw", "raster", "gfc", "GFC_cover_Colombia_mosaic.tif"))
 
 fc_agg <- fc %>%
-  terra::aggregate(fact = 10, cores = 4) %>%
-  project(crs(grid))
+  terra::aggregate(fact = 3, cores = 4) %>%
+  project(crs(country_ea)) %>%
+  mask(country_ea)
 
-extract_fc <- function(grid, fc_layer, fc_threshold = NULL, fc_values = NULL) {
-  
-  # Convert input layer to binary forest/nonforest
-  if (!is.null(fc_values)) {
-    fc_layer <- fc_layer %in% fc_values
-  } else if (!is.null(fc_values)) {
-    fc_layer <- (fc_layer >= fc_threshold)
-  }
-  
-  # Extract mean cover per polygon
-  # NB: Not weighted by overlapping pixel area (fine for small pixel size)
-  grid <- vect(grid)
-  grid_fc <- terra::extract(fc_layer, grid, fun = mean, bind = TRUE)
-  
-  st_as_sf(grid_fc) %>%
-    rename(forest_cover = 1)
-}
+fc_threshold <- as.numeric(fc_agg > 30)
 
-grid_fc <- extract_fc(grid, fc_agg$Y2000) %>%
-  rename("forest_cover" = 1)
+non_forest <- fc_threshold == 0
 
-grid_fc_threshold <- dplyr::filter(grid_fc, forest_cover > 40)
+# Detect patches
+
+tictoc::tic()
+non_forest_patches <- landscapemetrics::get_patches(
+  fc_threshold$Y2000,
+  class = 0,
+  to_disk = TRUE
+)[[1]][[1]]
+tictoc::toc()
+
+patch_area <- lsm_p_area(non_forest_patches)
+
+writeRaster(fc_agg, here("data", "processed", "raster", "fc_agg.tif"))
+
+## Filter polygons to those with > FC_THRESHOLD forest cover --------
+
+grid_fc <- extract_fc(grid, fc_agg$Y2000)
+
+grid_fc_threshold <- dplyr::filter(grid_fc, forest_cover > FC_THRESHOLD)
 
 ## Generate random sample polygons --------
-set.seed(111)
+set.seed(SAMPLE_SEED)
 
-sample_polygons <- function(x, n) {
-  if (n >= nrow(x)) {
-    warning("Requested n exceeds number of polygons in x")
-  }
-  slice_sample(x, n = n)
-}
-
-grid_sample <- sample_polygons(grid_fc_threshold, SAMPLE_N)
+grid_sample <- sample_polygons(grid_fc_threshold, n = SAMPLE_N)
 
 # Visualise
 
@@ -152,9 +81,15 @@ tm_shape(country) +
   tm_shape(fc_agg_country$Y2000) +
   tm_raster(col.scale = tm_scale_continuous(values = "Greens")) +
   tm_shape(grid_sample) +
-  tm_borders("red", lwd = 2)
+  tm_borders("orange", lwd = 2)
 
-hist(grid_sample$forest_cover, breaks = 100)
+## Save to disk
+output_name <- paste(COUNTRY, POLY_SHAPE, FC_THRESHOLD, POLY_SIZE, sep = "_")
 
-## 
+st_write(
+  grid_sample,
+  paste0(OUTPUT_PATH, "/", output_name),
+  driver = "GeoJSON",
+  delete_dsn = TRUE
+)
 
