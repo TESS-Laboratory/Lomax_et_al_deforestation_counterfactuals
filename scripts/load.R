c3fe6f7f5d08136be7d2c3687c337c0e78930722
Guy Lomax <G.Lomax@exeter.ac.uk>
2024-04-19 14:40:48 +0100

    Created separate script to load packages and functions.

---
+++ scripts/load.R
@@ -0,0 +1,19 @@
+reload <- function() {
+  # load all the packages you need for the project
+  message("Loading packages...")
+  suppressPackageStartupMessages(
+    source("scripts/packages.R")
+  )
+  # make the data dir if it doesn't exist
+  if (!file.exists("data")) {
+    message("Creating data directory...")
+    dir.create("data")
+  }
+  # read the functions from R directory
+  message("Loading functions...")
+  function_files <- list.files("R", pattern = ".R$", full.names = TRUE)
+  walk(function_files, source)
+  message("Complete")
+}
+
+reload()
