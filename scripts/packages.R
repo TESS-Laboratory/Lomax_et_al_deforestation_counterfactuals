8283a26948a21f1ded37e57e7c3e5762d8b53467
Guy Lomax <G.Lomax@exeter.ac.uk>
2024-04-19 14:42:09 +0100

    Package script

---
+++ scripts/packages.R
@@ -0,0 +1,19 @@
+# data reading and management
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(sf)
+library(terra)
+library(units)
+
+# data analysis
+library(exactextractr)
+library(landscapemetrics)
+
+# data visualisation
+library(tmap)
+library(ggplot2)
+
+# time tracking
+library(tictoc)
