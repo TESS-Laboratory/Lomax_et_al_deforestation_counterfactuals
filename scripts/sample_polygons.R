e7a1a4bd87752418a7f88da6ac2d68c163cf0a5e
Guy Lomax <G.Lomax@exeter.ac.uk>
2024-04-19 14:43:34 +0100

    Began converting script to functional style.
    Begin script with source("load.R").
    
    Added code to generate buffers for polygon grid and extract forest cover and forest cover loss.

--- scripts/sample_polygons.R
+++ scripts/sample_polygons.R
@@ -1,58 +1,71 @@
 ## Script to generate and sample hexagonal or square grids of a specified area
 ## and minimum forest cover within a country polygon.
 
-library(sf)
-library(terra)
-library(landscapemetrics)
-library(here)
-library(dplyr)
-library(tmap)
-library(tictoc)
-
-source(here("scripts", "R", "generate_polygons.R"))
+## Setup packages and functions --------
+source("scripts/load.R")
 
 ## Set parameters --------
 
-COUNTRY <- "Colombia"
-POLY_SIZE <- 10000 # hectares
-POLY_SHAPE <- "hex"  # square or hex
-SAMPLE_N <- 1000  # number of sample polygons
-FC_THRESHOLD <- 40  # Minimum forest cover to include polygons in sample (%)
-FOREST_EDGE_AREA <- 100000 # Minimum contiguous nonforest area to define as forest edge, hectares
-OUTPUT_PATH <- here("data", "processed", "vector", "sample_polygons")
-SAMPLE_SEED <- 111
+COUNTRY <- "Colombia"  # Target country
+CRS <- "ESRI:54034"  # CRS to generate grid
+COUNTRY_BUFFER <- 10 # buffer distance from country border to exclude when generating polygons (km)
+POLY_SIZE <- 100000 # size of polygons in hectares
+POLY_SHAPE <- "hex"  # square or hex polygon
+POLY_BUFFER <- 10 # polygon buffer distance in km
+SAMPLE_N <- 500  # number of polygons to sample
+FC_THRESHOLD <- 20  # Minimum forest cover to include polygons in sample (%)
+FOREST_EDGE_AREA <- 100 # Minimum continuous nonforest area to define as forest edge, hectares
+OUTPUT_PATH <- "data/processed/vector/sample_polygons"  # Output directory
+SEED <- 111  # Random number seed
 
-## Load country polygon --------
+AGG <- 5 # Factor to aggregate rasters to speed calculation/test pipeline
 
-path <- here("data", "raw", "vector", "WB_countries_Admin0_10m", "WB_countries_Admin0_10m.shp")
-country <- get_country(path, country_name = COUNTRY)
+## Load country polygon and project to equal area projection--------
 
-# Convert to equal area projection (Cylindrical equal area)
-country_ea <- st_transform(country, crs = "ESRI:54034")
+country <- get_country(COUNTRY, crs = CRS)
 
 ## Generate polygon grids --------
 
-grid <- generate_polygons(country_ea, shape = POLY_SHAPE, area = POLY_SIZE)
+tic()
+grid <- generate_polygons(
+  country,
+  buffer = COUNTRY_BUFFER,
+  shape = POLY_SHAPE,
+  area = POLY_SIZE
+)
+toc()
 
-## Calculate distance to forest edge per pixel --------
+grid_buffer <- generate_buffers(grid, dist = POLY_BUFFER)
 
-# Load forest cover raster
-# Raster should be "closed", i.e., not contain small holes
-fc <- rast(here("data", "raw", "raster", "gfc", "GFC_cover_Colombia_mosaic.tif"))
+## Calculate FC per polygon and filter to those with > FC_THRESHOLD --------
 
-# fc_agg <- fc %>%
-#   terra::aggregate(fact = 3, cores = 8) %>%
-#   project(crs(country_ea)) %>%
-#   mask(country_ea)
-# writeRaster(fc_agg, here("data", "processed", "raster", "fc_agg.tif"))
+# Load forest cover raster and convert to binary forest/nonforest
+fc <- get_raster(COUNTRY, folder = "gfc", layer = "treecover2000") %>%
+  aggregate(fact = AGG) %>%
+  is_greater_than(50) %>%
+  as.numeric()
 
-fc_agg <- rast(here("data", "processed", "raster", "fc_agg.tif"))
+# Extract forest cover per polygon and filter to 
+grid_fc <- grid %>%
+  poly_extract(fc) %>%
+  filter(treecover2000 > (FC_THRESHOLD / 100))
 
-fc_threshold <- as.numeric(fc_agg > 30)
+grid_buffer_fc <- grid_buffer %>%
+  filter(ID %in% grid_fc$ID) %>%
+  poly_extract(fc)
 
-# non_forest <- fc_threshold == 0
+## Extract forest loss per polygon
 
-# Detect patches
+fc_loss <- get_raster(COUNTRY, folder = "gfc", layer = "lossyear")
+
+grid_fc_loss <- poly_extract(grid_fc[1:20,], fc_loss, fun = sum_by_value)
+grid_buffer_fc_loss <- poly_extract(grid_buffer_fc[1:20,], fc_loss, fun = sum_by_value)
+
+## Generate random sample polygons --------
+set.seed(SAMPLE_SEED)
+
+## Add distance from forest edge to polygon properties --------
+# Detect non-forest patches
 
 tic()
 non_forest_patches <- landscapemetrics::get_patches(
@@ -61,6 +74,8 @@ non_forest_patches <- landscapemetrics::get_patches(
   to_disk = TRUE
 )[[1]][[1]]
 toc()
+
+names(non_forest_patches) <- "patch"
 
 # tic()
 # patch_area <- fc_threshold$Y2000 %>%
@@ -72,42 +87,47 @@ toc()
 # readr::write_rds(patch_area, here("data", "processed", "patch_area_Colombia.rds"))
 
 # Reclassify nonforest patch values to area
-patch_area <- readr::read_rds(here("data", "processed", "patch_area_Colombia.rds"))
-patch_area_large <- filter(patch_area, value >= FOREST_EDGE_AREA)
+patch_area_large <- readr::read_rds(here("data", "processed", "patch_area_Colombia.rds")) %>%
+  filter(value >= FOREST_EDGE_AREA)
 
 tic()
-non_forest_patch_area <- classify(non_forest_patches, rcl = patch_area_large)
+non_forest_patch_area <- classify(non_forest_patches, rcl = patch_area_large, others = NA)
 toc()
 
-## Filter polygons to those with > FC_THRESHOLD forest cover --------
+# Calculate distance to forest edge for all forest pixels
+tic()
+dist_to_edge <- terra::distance(non_forest_patch_area)
+toc()
 
-grid_fc <- extract_fc(grid, fc_agg$Y2000)
+names(dist_to_edge) <- "dist_to_edge"
 
-grid_fc_threshold <- dplyr::filter(grid_fc, forest_cover > FC_THRESHOLD)
-
-## Generate random sample polygons --------
-set.seed(SAMPLE_SEED)
-
-grid_sample <- sample_polygons(grid_fc_threshold, n = SAMPLE_N)
+# Extract mean distance to edge for all sample polygons
+tic()
+grid_fc_edge <- extract_grid(grid_sample, dist_to_edge)
+toc()
 
 # Visualise
 
 fc_agg_country <- mask(crop(fc_agg, country_ea), country_ea)
+dist_to_edge_country_mask <- dist_to_edge %>%
+  mask(country_ea) %>%
+  mask(dist_to_edge > 0, maskvalues = c(0, NA))
 
-tm_shape(country) +
-  tm_fill("grey95") +
-  tm_shape(fc_agg_country$Y2000) +
+tmap_mode("view")
+
+tm_shape(fc_agg_country$Y2000) +
   tm_raster(col.scale = tm_scale_continuous(values = "Greens")) +
+  tm_shape(dist_to_edge_country_mask) +
+  tm_raster(col.scale = tm_scale_continuous(values = "viridis")) +
   tm_shape(grid_sample) +
-  tm_borders("orange", lwd = 2)
+  tm_borders("black", lwd = 2)
 
 ## Save to disk
-output_name <- paste(COUNTRY, POLY_SHAPE, FC_THRESHOLD, POLY_SIZE, sep = "_")
+output_name <- paste(COUNTRY, POLY_SHAPE, POLY_SIZE, sep = "_")
 
 st_write(
-  grid_sample,
-  paste0(OUTPUT_PATH, "/", output_name),
-  driver = "GeoJSON",
+  grid_fc_edge,
+  paste0(OUTPUT_PATH, "/", output_name, ".geojson"),
   delete_dsn = TRUE
 )
 
