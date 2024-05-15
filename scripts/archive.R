f2c848e7020ee1a87f19e7749c36add0604b04c1
Guy Lomax <G.Lomax@exeter.ac.uk>
2024-05-15 11:12:29 +0100

    Cleaned up distance to forest edge script

--- scripts/archive.R
+++ scripts/archive.R
@@ -4,31 +4,29 @@
 
 #### Calculating distance from forest edge for each grid cell (2024-05-01)
 
-## Add distance from forest edge to polygon properties --------
+## Calculate mean distance from forest edge for each polygon --------
 # Detect non-forest patches
 
 tic()
 non_forest_patches <- landscapemetrics::get_patches(
-  fc_threshold$Y2000,
-  class = 0,
-  to_disk = TRUE
+  fc_threshold$Y2000,  # Binary forest/non-forest raster
+  class = 0,  # Non-forest class
+  to_disk = TRUE  # Saves memory
 )[[1]][[1]]
 toc()
 
 names(non_forest_patches) <- "patch"
 
-# tic()
-# patch_area <- fc_threshold$Y2000 %>%
-#   lsm_p_area() %>%
-#   filter(class == 0) %>%
-#   select(id, value)
-#   
-# toc()
-# readr::write_rds(patch_area, here("data", "processed", "patch_area_Colombia.rds"))
+tic()
+patch_area <- fc_threshold$Y2000 %>%
+  lsm_p_area() %>%
+  filter(class == 0) %>%
+  select(id, value)
+
+toc()
 
 # Reclassify nonforest patch values to area
-patch_area_large <- readr::read_rds(here("data", "processed", "patch_area_Colombia.rds")) %>%
-  filter(value >= FOREST_EDGE_AREA)
+patch_area_large <- filter(patch_area, value >= FOREST_EDGE_AREA)
 
 tic()
 non_forest_patch_area <- classify(non_forest_patches, rcl = patch_area_large, others = NA)
@@ -40,6 +38,8 @@ dist_to_edge <- terra::distance(non_forest_patch_area)
 toc()
 
 names(dist_to_edge) <- "dist_to_edge"
+
+plot(dist_to_edge)
 
 # Extract mean distance to edge for all sample polygons
 tic()
