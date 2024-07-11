2b2324a1748279f906a31da87c4fa582b51d9c9f
G-Lomax <G.Lomax@exeter.ac.uk>
2024-07-11 14:00:25 +0100

    Simplified raster merging script.

--- scripts/stitch_rasters.R
+++ scripts/stitch_rasters.R
@@ -1,21 +1,19 @@
 # Script to mosaic tiled rasters
 
-library(terra)
 library(sf)
 
-# Load
+COUNTRY <- "Colombia"
 
+# Load
 folder_path <- "data/raw/raster/tmf"
-file_prefix <- "TMFAnnualClasses_Colombia"
-files <- Sys.glob(paste0(folder_path, "/", file_prefix, "*.tif"))
+files <- Sys.glob(paste0(folder_path, "/*", COUNTRY, "*.tif"))
+folder_out <- "data/processed/raster/tmf"
 
 # Warp and write merged file
 tictoc::tic()
 gdal_utils(
   util = "warp",
   source = files,
-  destination = paste0(folder_path, "/", file_prefix, "_merged.tif")
+  destination = paste0(folder_out, "/tmf_", COUNTRY, "_merged.tif")
 )
 tictoc::toc()
-
-writeRaster(mosaic, paste0(folder_path, "/", file_prefix, ".tif"))
