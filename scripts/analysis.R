ea3d730b8e73fc525d83198c5062a4e1e775d710
G-Lomax <G.Lomax@exeter.ac.uk>
2025-10-21 14:11:03 +0100

    Added command line instructions.
    Removed matching period from setup code and added start year to command line args.
    
    Improved code comments.

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -2,31 +2,51 @@
 #### Fits augmented synthetic controls for sample polygon data and writes the
 #### results to disk as a CSV.
 
+#### Guy Lomax
+#### G.Lomax@exeter.ac.uk
+
+# Instructions
+## To run from terminal go to project directory and use syntax:
+
+# "nohup Rscript scripts/analysis.R [COUNTRY] [POLY_SIZE] [START_YEAR] [MAX_POOL] [N_CORES] [ECON] [CUMULATIVE] &> [out_file] &"
+# e.g.,
+# "nohup Rscript scripts/analysis.R "Cote d'Ivoire" 60000 1000 2016 16 1 0 &> cotedivoire.out &"
+# logicals should be entered as 1/0, not TRUE/FALSE or T/F
+
 source("scripts/load.R")
 
 ### 1. Set parameters --------
 
 # Data selection
-COUNTRY <- "Malaysia"  # Target country
-START_YEAR <- 2016 # Simulated start year of protection project
-MATCHING_PERIODS <- 8  # Length of pre-intervention period to use for matching (years)
-POLY_SIZE <- 60000 # size of polygons in hectares
-SIMULATIONS <- c(1,2,3,4,5)  # Simulations to run
-ECON <- FALSE  # Economic data present for country
-SEED <- 1471
-MAX_POOL <- 1000  # Max number of potential donor polygons to constrain 
-N_CORES <- 16
-CUMULATIVE <- FALSE  # Use cumulative rather than annual deforestation to fit
+COUNTRY <- "Malaysia"       # Target country
+START_YEAR <- 2016          # Simulated start year of protection project
+POLY_SIZE <- 60000          # size of polygons in hectares
+SIMULATIONS <- c(1,2,3,4,5) # Simulations to run
+ECON <- FALSE               # Is economic data present for this country?
+SEED <- 1471                # Random seed
+MAX_POOL <- 1000            # Max number of potential donor polygons to constrain 
+N_CORES <- 16               # Number of cores for parallelisation 
+CUMULATIVE <- FALSE         # Fit to cumulative rather than annual loss?
 
-# Simulations to run for RQ1 and RQ3
+# Simulations to run for different RQs
 if (START_YEAR == 2016) {
+  
   if (POLY_SIZE == 60000) {
+    
+    # RQ1 & RQ3
     simulation_match_df <- read_csv("data/raw/csv/simulation_list_60000.csv")
+    
   } else {
+    
+    # RQ2
     simulation_match_df <- tibble(sim = 5, match = 8)
+    
   }
 } else if (START_YEAR == 1998) {
+  
+  # RQ4
   simulation_match_df <- tibble(sim = SIMULATIONS, match = 8)
+  
 }
 
 simulation_match_df <- filter(simulation_match_df, sim %in% SIMULATIONS)
@@ -34,23 +54,24 @@ simulation_match_df <- filter(simulation_match_df, sim %in% SIMULATIONS)
 # Get parameters from command line if running from terminal
 cmd_args <- commandArgs(TRUE)
 
-if (length(cmd_args) == 6) {
+if (length(cmd_args) == 7) {
   COUNTRY <- cmd_args[1]
   POLY_SIZE <- as.numeric(cmd_args[2])
-  MAX_POOL <- as.numeric(cmd_args[3])
-  N_CORES <- as.numeric(cmd_args[4])
-  ECON <- as.logical(as.numeric(cmd_args[5]))
-  CUMULATIVE <- as.logical(as.numeric(cmd_args[6]))
+  START_YEAR <- as.numeric(cmd_args[3])
+  MAX_POOL <- as.numeric(cmd_args[4])
+  N_CORES <- as.nhtopumeric(cmd_args[5])
+  ECON <- as.logical(as.numeric(cmd_args[6]))
+  CUMULATIVE <- as.logical(as.numeric(cmd_args[7]))
 } else {
   print("Insufficient command line arguments given - using default values")
 }
 
 cat("Fitting synthetic controls for ", COUNTRY, "- Start Year: ", START_YEAR, ", Polygon Size: ", POLY_SIZE, " ha")
 
-# # Parallelisation
-# if (N_CORES > 1) {
-#   plan(multicore, workers = N_CORES)
-# }
+# Parallelisation
+if (N_CORES > 1) {
+  plan(multicore, workers = N_CORES)
+}
 
 ### 2. Load data
 grid_data <- read_rds(paste0("data/processed/rds/", COUNTRY, "_", POLY_SIZE, "_", START_YEAR, "_data.rds"))
@@ -125,6 +146,7 @@ sc_df <- sc_results %>%
   select(-synth) %>%
   unnest(sc_results)
 
+# Save output
 cumulative_flag <- ifelse(CUMULATIVE == TRUE, "_cumulative", "")
 
 output_filename <- paste0("sc_results_", COUNTRY, "_", START_YEAR, "_", POLY_SIZE, cumulative_flag, ".csv")
@@ -133,5 +155,6 @@ write_csv(sc_df, paste0("results/sc_results/", output_filename))
 
 cat("Results written to ", output_filename)
 
+# Send notification
 pushover(message = paste0("SC analysis complete: ", COUNTRY, ". Start year: ", START_YEAR))
 
