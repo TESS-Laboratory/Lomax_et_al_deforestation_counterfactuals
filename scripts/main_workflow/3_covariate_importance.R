51fd710a157be3bbd69f3fc7f180832591902e5d
G-Lomax <G.Lomax@exeter.ac.uk>
2026-06-08 16:23:47 +0100

    Added code for donor pool filtering.
    Included internal check for econ variables to avoid need to manually specify.

--- scripts/main_workflow/3_covariate_importance.R
+++ scripts/main_workflow/3_covariate_importance.R
@@ -7,24 +7,27 @@ source("scripts/load.R")
 ### 1. Set parameters --------
 
 # Data selection
-COUNTRY <- "Bolivia"  # Target country
+COUNTRY <- "Colombia"  # Target country
 START_YEAR <- 2016 # Simulated start year of protection project
 MATCHING_PERIOD <- 8  # Length of pre-intervention period to use for matching (years)
 POLY_SIZE <- 60000 # size of polygons in hectares
-SIMULATIONS <- 5  # Simulations to run
-ECON <- TRUE  # Economic data present for country
 SEED <- 1471
-MAX_POOL <- 100  # Max number of potential donor polygons to constrain 
-N_CORES <- 8
+MAX_POOL <- 1000  # Max number of potential donor polygons to constrain
+MIN_POOL <- 20  # Minimum donors to proceed with fitting
+DONOR_FILTER <- TRUE  # Filter donor pool based on prior buffer deforestation rates?
+FILTER_START <- 2009  # Start year for calculating prior buffer deforestation rates
+FILTER_THRESHOLD <- 0.1  # fractional range to filter prior buffer deforestation rates
+N_CORES <- 1
 
 # Get parameters from command line if running from terminal
 cmd_args <- commandArgs(TRUE)
 
-if (length(cmd_args) == 4) {
+if (length(cmd_args) == 5) {
   COUNTRY <- cmd_args[1]
-  MAX_POOL <- as.numeric(cmd_args[2])
-  N_CORES <- as.numeric(cmd_args[3])
-  ECON <- as.logical(as.numeric(cmd_args[4]))
+  POLY_SIZE <- as.numeric(cmd_args[2])
+  START_YEAR <- as.numeric(cmd_args[3])
+  MAX_POOL <- as.numeric(cmd_args[4])
+  N_CORES <- as.numeric(cmd_args[5])
 } else {
   print("Insufficient command line arguments given - using default values")
 }
@@ -37,36 +40,38 @@ if (N_CORES > 1) {
 }
 
 ### 2. Load data
-## To fix tomorrow - now have the geoms attached to grid_data, which is in wide format
-## so need to extract them and then again pivot_longer and separate_wider_delim...
 grid_data <- read_rds(paste0("data/processed/rds/", COUNTRY, "_", POLY_SIZE, "_", START_YEAR, "_data.rds"))
+
+# Test if econ data is present in dataset
+econ <- max(str_detect(colnames(grid_data), "grp_pc_usd_2015|ag_grp_frac"))
 
 ### 3. Prepare data and run analysis
 
 sample_ids <- grid_data %>%
   st_drop_geometry() %>%
   select(ID, stratum) %>%
-  filter(!is.na(stratum))
+  filter(!is.na(stratum)) %>%
+  mutate(n_donors = NA, final_filter_range = NA)
 
-# Reduce potential donors for large donor pools
-if (nrow(grid_data) > MAX_POOL) {
-  message("Donor pool too large; reducing to ", MAX_POOL)
-  
-  n_sample <- nrow(sample_ids)
-  grid_data_sample <- filter(grid_data, ID %in% sample_ids$ID)
-  
-  set.seed <- SEED
-  n_pool <- MAX_POOL - n_sample
-  grid_data_pool <- grid_data %>%
-    filter(!(ID %in% sample_ids$ID)) %>%
-    slice_sample(n = n_pool)
-  
-  grid_data <- bind_rows(grid_data_sample, grid_data_pool)
-}
+# # Reduce potential donors for large donor pools
+# if (nrow(grid_data) > MAX_POOL) {
+#   message("Donor pool too large; reducing to ", MAX_POOL)
+#   
+#   n_sample <- nrow(sample_ids)
+#   grid_data_sample <- filter(grid_data, ID %in% sample_ids$ID)
+#   
+#   set.seed <- SEED
+#   n_pool <- MAX_POOL - n_sample
+#   grid_data_pool <- grid_data %>%
+#     filter(!(ID %in% sample_ids$ID)) %>%
+#     slice_sample(n = n_pool)
+#   
+#   grid_data <- bind_rows(grid_data_sample, grid_data_pool)
+# }
 
 # Loop through sample to perform analysis
 set.seed(SEED)
-sc_results <- future_map(sample_ids$ID, .options = furrr_options(seed = TRUE), .progress = TRUE, function(id) {
+sc_results <- future_map(sample_ids$ID, function(id) {
   
   # Add additional variables specific to treated unit
   grid_data_sample <- grid_data %>%
@@ -78,11 +83,54 @@ sc_results <- future_map(sample_ids$ID, .options = furrr_options(seed = TRUE), .
     select(-stratum, -cum_loss) %>%
     drop_na()  # Remove donors with NA values in key variables
   
-  # Prepare data to format required by augsynth
+  # Prepare data to format required by synth methods
   
   grid_data_prepared <- grid_data_sample %>%
     wide_to_long() %>%
     mutate(treated = treated * (year > START_YEAR))
+  
+  # Pre-filter based on prior buffer deforestation if DONOR_FILTER == TRUE
+  
+  if (DONOR_FILTER == TRUE) {
+    buffer_prior_defor <- grid_data_prepared %>%
+      filter(year >= FILTER_START & year <= START_YEAR) %>%
+      group_by(ID) %>%
+      summarise(prior_buffer_loss = sum(buffer_loss))
+    
+    # Treated unit deforestation
+    buffer_prior_defor_treated <- buffer_prior_defor$prior_buffer_loss[buffer_prior_defor$ID == id]
+    
+    # Filter donors according to threshold; increase threshold if insufficient donors identified
+    filter_range <- FILTER_THRESHOLD
+    
+    sufficient_donors <- FALSE
+    
+    donors <- filter(buffer_prior_defor, ID != id)
+    
+    while(sufficient_donors == FALSE) {
+      message("Filtering donors on prior deforestation rate - buffer = ", filter_range * 100, "%\n")
+      
+      donors_filtered <- donors %>%
+        filter(prior_buffer_loss >= buffer_prior_defor_treated * (1 - filter_range) &
+                 prior_buffer_loss <= buffer_prior_defor_treated * (1 + filter_range))
+      
+      n_donors <- nrow(donors_filtered)
+      
+      # Escape while loop if all donors are already included
+      if (n_donors == nrow(buffer_prior_defor) - 1 | nrow(buffer_prior_defor) < MIN_POOL) {
+        break
+      }
+      
+      sufficient_donors <- n_donors >= MIN_POOL
+      
+      filter_range <- filter_range + FILTER_THRESHOLD
+    }
+    
+    message("Donor filtering complete\n")
+    
+    grid_data_prepared <- filter(grid_data_prepared, ID %in% c(id, donors_filtered$ID))
+    
+  }
   
   # Run synthetic controls for different simulations
   
@@ -95,7 +143,8 @@ sc_results <- future_map(sample_ids$ID, .options = furrr_options(seed = TRUE), .
     mutate(synth = map(id, run_synthetic_control_mscmt, match = MATCHING_PERIOD, data = grid_data_prepared))
     
   sim_df
-})
+},
+.options = furrr_options(seed = TRUE), .progress = TRUE)
 
 ### 4. Combine results and summarise data
 
