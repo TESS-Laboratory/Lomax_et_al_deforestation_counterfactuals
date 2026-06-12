9bee5cc6542efae1dc2969f6b2ce075f28bd7906
Guy Lomax <G.Lomax@exeter.ac.uk>
2026-06-12 08:56:01 +0100

    Updated to remove obsolete read_renoster() function.

--- scripts/main_workflow/0_clean_redd_polys.R
+++ scripts/main_workflow/0_clean_redd_polys.R
@@ -5,8 +5,6 @@ library(dplyr)
 library(readr)
 library(sf)
 
-source("R/read_data.R")
-
 sf_use_s2(FALSE)
 
 renoster <- read_csv("data/raw/csv/redd_projects/carbon_projects_database_index.csv")
@@ -17,20 +15,17 @@ renoster_ad <- renoster %>%
   filter(`Project Type` == "AD") %>%
   select(all_of(cols_keep_renoster))
 
-# Load, clean and combine Renoster polygon data
-africa_polys <- read_renoster(
-  "data/raw/vector/redd_polygons/africa.gpkg",
-  id_list = renoster_ad$ProjectID)
-asia_polys <- read_renoster(
-  "data/raw/vector/redd_polygons/asia.gpkg",
-  id_list = renoster_ad$ProjectID)
-south_america_polys <- read_renoster(
-  "data/raw/vector/redd_polygons/south_america.gpkg",
-  id_list = renoster_ad$ProjectID
-)
+# Load, combine and clean Renoster polygon data
+africa_polys <- st_read("data/raw/vector/redd_polygons/africa.gpkg") %>%
+  filter(ProjectID %in% renoster_ad$ProjectID)
+asia_polys <- st_read("data/raw/vector/redd_polygons/asia.gpkg") %>%
+  filter(ProjectID %in% renoster_ad$ProjectID)
+south_america_polys <- st_read("data/raw/vector/redd_polygons/south_america.gpkg") %>%
+  filter(ProjectID %in% renoster_ad$ProjectID)
 
 renoster_polys <- bind_rows(africa_polys, asia_polys, south_america_polys) %>%
   inner_join(renoster_ad, by = "ProjectID") %>%
-  st_make_valid()
+  mutate(geom = purrr::map(geom, st_make_valid) %>%
+           st_as_sfc(crs = st_crs(africa_polys)))
 
 st_write(renoster_polys, "data/processed/vector/renoster_polys.gpkg", delete_dsn = TRUE)
